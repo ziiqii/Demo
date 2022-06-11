4eb593734a12e1e600078a3c66970aa3c316d722
Ziqi <ziiiqiii@gmail.com>
2022-06-12 00:53:16 +0800

    added tab icons

--- App.js
+++ App.js
@@ -5,16 +5,37 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import ContactScreen from './screens/ContactScreen';
+import { FontAwesome } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name='Home' component={HomeScreen} />
-        <Tab.Screen name='Settings' component={SettingsScreen} />
-        <Tab.Screen name='Contact' component={ContactScreen} />
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            //Set the icon based on which route it is (name of the tab)
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Events') {
+              iconName = 'list';
+            } else if (route.name === "Contacts") {
+              iconName = "user";
+            }
+
+            // You can return any component that you like here!
+            return <FontAwesome name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'hotpink',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Events" component={SettingsScreen} />
+        <Tab.Screen name="Contacts" component={ContactScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home!</Text>
+      <FontAwesome name="home" size={50} color="red" /> {/*Self closing tag*/}
     </View>
   );
 }
